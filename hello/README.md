7988af64f56e33f4b2ba73a66627c038a4aec799
voje <voje@iskratel.si>
2023-01-19 13:47:05 +0100

    hello 0.0.2

--- hello/README.md
+++ hello/README.md
@@ -3,3 +3,5 @@
 Simple hello server that returns whatever we define in the 
 `HELLO` env variable.   
 
+Optionally set route with `HELLO_ROUTE`.   
+
--- hello/hello.go
+++ hello/hello.go
@@ -14,9 +14,17 @@ func getHelloEnv() string {
 	return val
 }
 
+func getHelloPathEnv() string {
+	val, ok := os.LookupEnv("HELLO_PATH")
+	if !ok {
+		return "/"
+	}
+	return val
+}
+
 func main() {
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
+	r.GET(getHelloPathEnv(), func(c *gin.Context) {
 		c.Data(http.StatusOK, "text/html; charset=utf-8", []byte(getHelloEnv()))
 	})
 	r.Run()
